1236a2ed675a9291d0d6eadbc0511031bbfa3f13
Rich Cartwright <richard.cartwright@hud.ac.uk>
2019-11-13 13:19:43 +0000

    updated plugins and added new plugins to the updater python script

--- update_plugins.py
+++ update_plugins.py
@@ -57,6 +57,9 @@ vim-abolish https://github.com/tpope/tpope-vim-abolish
 mru.vim https://github.com/vim-scripts/mru.vim
 vim-markdown https://github.com/plasticboy/vim-markdown
 comfortable-motion.vim https://github.com/yuttie/comfortable-motion.vim
+vimtex https://github.com/larvag/vimtex
+syntastic https://github.com/vim-syntastic/syntastic
+ag.vim https://github.com/rking/ag.vim
 """.strip()
 
 GITHUB_ZIP = '%s/archive/master.zip'
